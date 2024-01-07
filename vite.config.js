7d40b6b33f1c3571f342d9610297f1baa0603081
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2024-01-07 19:57:01 +0530

    vite 🔚

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define:{"process.env.REACT_APP_API_URL": JSON.stringify("http://localhost:4500")},
+  define:{"process.env.REACT_APP_API_URL": JSON.stringify("https://userauth-tsuw.onrender.com")},
   plugins: [react()],
 })
