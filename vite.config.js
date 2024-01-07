aa9f66e1869db3b835f188547044a2d5021bce84
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2024-01-07 20:38:39 +0530

    secretfile added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define:{"process.env.REACT_APP_API_URL": JSON.stringify("https://userauth-tsuw.onrender.com")},
+  define:{"process.env.REACT_APP_API_URL": JSON.stringify(`${process.env.REACT_APP_API_URL}`)},
   plugins: [react()],
 })
